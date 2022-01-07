654bfe94850a3d488ec70c29e36c8dbaa9da1b9c
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2022-01-07 15:49:51 +0200

    feat: added standard dependencies for the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,37 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
   env: {
+    commonjs: true,
     browser: false,
     es2021: true,
     mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint', '@fintechstudios/eslint-plugin-chai-as-promised'],
   extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@fintechstudios/chai-as-promised/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:import/recommended',
+    'plugin:node/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-  },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
+    'no-empty': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'require-await': 'error',
+    '@fintechstudios/chai-as-promised/no-unhandled-promises': 'error',
+    '@fintechstudios/chai-as-promised/no-await-in-condition': 'error',
+    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+    'node/no-unpublished-import': 'off',
+    'node/no-missing-import': 'off',
   },
 };
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,55 @@
-import * as dotenv from "dotenv";
+import '@typechain/hardhat';
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'hardhat-contract-sizer';
+import 'hardhat-deploy';
 
-dotenv.config();
+import dotenv from 'dotenv';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const env = dotenv.config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const HH_MNEMONIC = 'test test test test test test test test test test test junk';
+const MNEMONIC = env.parsed?.MNEMONIC || HH_MNEMONIC;
+const ETHERSCAN_API_KEY = env.parsed?.ETHERSCAN_API;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  mocha: {
+    bail: true,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.11',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.ROPSTEN_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    currency: 'USD',
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: './typechain',
+    target: 'ethers-v5',
   },
 };
 
