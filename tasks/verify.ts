8c22a4f0066b938a7e508d53037405f75de5c80d
Stanislav Chernenko <stanis.chernes@yandex.ru>
2022-08-05 14:11:42 +0300

    feat: updated verification script in accordance to new task fields + new .env parameter

--- tasks/verify.ts
+++ tasks/verify.ts
@@ -1,4 +1,38 @@
 import { task, types } from 'hardhat/config';
+import { config as dotenvConfig } from 'dotenv';
+import { promises as fs } from 'fs';
+
+const env = dotenvConfig();
+const ABSOLUTE_PROJECT_PATH = env.parsed?.ABSOLUTE_PROJECT_PATH as string;
+
+// eslint-disable-next-line consistent-return
+export const writeLibrariesToFile = async (contractName: string, libraries: object): Promise<string | undefined> => {
+  const librariesFilePath = `${ABSOLUTE_PROJECT_PATH}/${contractName}-libraries.js`;
+  const librariesObject = `module.exports = ${JSON.stringify(libraries)}`;
+
+  console.log(`Writing { ${librariesObject} } to ${librariesFilePath}`);
+
+  try {
+    await fs.writeFile(librariesFilePath, librariesObject);
+    return librariesFilePath;
+  } catch (err) {
+    console.error(`Error occured while writing libraries to file for ${contractName} contract: `, err);
+  }
+};
+
+class ContractWithDynamicLibraryVerificationError extends Error {
+  constructor(contractName: string, libraries: object) {
+    super(`Failed verifying libraries set { ${JSON.stringify(libraries)} } for ${contractName} contract`);
+    Object.setPrototypeOf(this, ContractWithDynamicLibraryVerificationError.prototype);
+  }
+}
+
+interface VerificationParams {
+  address: string;
+  constructorArguments: any[];
+  libraries: string | undefined;
+  contract: string | undefined;
+}
 
 task('verification:verify', 'Verification of a contract for a different chain scanners')
   .addParam('contractName', 'The name of the contract', undefined, types.string, false)
@@ -6,24 +40,39 @@ task('verification:verify', 'Verification of a contract for a different chain sc
   .addParam('constructorArguments', 'The arguments passed to constructor during deploy', undefined, types.json, false)
   .addParam('proxyVerification', 'Usage of old verify task', undefined, types.boolean, false)
   .addParam('contractLibraries', 'The set of libraries used by contract', undefined, types.json, true)
+  .addParam('contractSpecifier', 'The contract Solidity specifier', undefined, types.string, true)
   .setAction(
-    async ({ contractName, contractAddress, constructorArguments, proxyVerification, contractLibraries }, hre) => {
+    async (
+      { contractName, contractAddress, constructorArguments, proxyVerification, contractLibraries, contractSpecifier },
+      hre,
+    ) => {
       try {
         console.log(`Verifying ${contractName} contract: ${contractAddress}`);
-        let verificationParams;
+        const verificationParams: VerificationParams = {
+          address: contractAddress as string,
+          constructorArguments: constructorArguments as [],
+          libraries: undefined,
+          contract: undefined,
+        };
 
         if (contractLibraries) {
-          verificationParams = {
-            address: contractAddress as string,
-            constructorArguments: constructorArguments as [],
-            libraries: contractLibraries as object,
-          };
-        } else {
-          verificationParams = {
-            address: contractAddress as string,
-            constructorArguments: constructorArguments as [],
-          };
+          const librariesFilePath: string | undefined = await writeLibrariesToFile(
+            contractName as string,
+            contractLibraries as object,
+          );
+
+          if (librariesFilePath) {
+            verificationParams.libraries = librariesFilePath;
+          } else {
+            throw new ContractWithDynamicLibraryVerificationError(contractName as string, contractLibraries as object);
+          }
         }
+
+        if (contractSpecifier) {
+          verificationParams.contract = contractSpecifier as string;
+        }
+
+        console.log(`Verification params ${JSON.stringify(verificationParams)}`);
 
         if (proxyVerification) {
           await hre.run('verify', verificationParams);
